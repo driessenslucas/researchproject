8d3eb17b076b49622fc9670fb20d126601127bb5
driessenslucas <lucas.driessens@student.howest.be>
2024-01-09 18:11:35 +0100

    fixed: sensor issues. added: code for stage1 rc-car

---
+++ arduino/sketch_jan09b/sketch_jan09b.ino
@@ -0,0 +1,89 @@
+// Editor     : Lauren from DFRobot
+// Date       : 17.02.2012
+
+// Product name: L298N motor driver module DF-MD v1.3
+// Product SKU : DRI0002
+// Version     : 1.0
+
+// Description:
+// The sketch for using the motor driver L298N
+// Run with the PWM mode
+
+// Connection:
+// M1 pin -> Digital pin 4
+// E1 pin -> Digital pin 5
+// M2 pin -> Digital pin 7
+// E2 pin -> Digital pin 6
+// Motor Power Supply -> Center blue screw connector (5.08mm 3p connector)
+// Motor A -> Screw terminal close to E1 driver pin
+// Motor B -> Screw terminal close to E2 driver pin
+//
+// Note: You should connect the GND pin from the DF-MD v1.3 to your MCU controller. They should share the GND pins.
+
+int E1 = 2;
+int M1 = 17;
+int E2 = 5;
+int M2 = 4;
+
+void setup() {
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
+    analogWrite(E2, 0); // Set speed
+    digitalWrite(M2, HIGH); // Move forward
+    Serial.begin(9600); // Initialize serial communication at 9600 bits per second
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    switch(command) {
+      case 'F':
+        Serial.println("moving forward");
+        move_forward();
+        break;
+      case 'L':
+        Serial.println("moving left");
+        move_left();
+        break;
+      case 'R':
+        Serial.println("moving right");
+        move_right();
+        break;
+      default:
+        Serial.println("standby");
+        stop_moving();
+        break;
+    }
+    delay(1000);
+  }
+}
+
+void move_forward() {
+  // Forward movement code
+  analogWrite(E1, 255); // Set speed
+  analogWrite(E2, 255); // Set speed
+  digitalWrite(M1, HIGH); // Move forward
+  digitalWrite(M2, LOW); // Move forward
+}
+
+void move_left() {
+  // Left turn code
+  analogWrite(E1, 255); // Set speed
+  analogWrite(E2, 0); // Stop one motor for turning
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+}
+
+void move_right() {
+  // Right turn code
+  analogWrite(E1, 0); // Stop one motor for turning
+  analogWrite(E2, 255); // Set speed
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
+}
+void stop_moving() {
+  analogWrite(E1, 0); // Stop one motor for turning
+  analogWrite(E2, 0); // Set speed
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, HIGH);
+}
---
+++ arduino/sketch_jan09d/sketch_jan09d.ino
@@ -0,0 +1,172 @@
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+// or for ESP32: #include <WiFi.h>
+
+const char* ssid = "telenet-799DCED";
+const char* password = "m7cnypsHjxhp";
+
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+int E1 = 2;
+int M1 = 17;
+int E2 = 19;
+int M2 = 4;
+
+int turnDuration = 130;
+
+void setup() {
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  analogWrite(E2, 0); // Set speed
+  digitalWrite(M2, HIGH); // Move forward
+
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+
+    ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  WiFiClient client = server.available(); // Listen for incoming clients
+
+  if (client) {
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            // Send a standard HTTP response header
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            // Send the HTML content
+            client.println("<!DOCTYPE html>");
+            client.println("<html>");
+            client.println("<body>");
+            client.println("<h2>ESP32 Motor Control</h2>");
+            client.println("<p><a href=\"/forward\"><button class=\"button\">Forward</button></a></p>");
+            client.println("<p><a href=\"/left\"><button class=\"button\">Left</button></a></p>");
+            client.println("<p><a href=\"/right\"><button class=\"button\">Right</button></a></p>");
+            client.println("<p><a href=\"/stop\"><button class=\"button button2\">Stop</button></a></p>");
+            client.println("</body>");
+            client.println("</html>");
+
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+        
+
+        // Check the request route
+        if (currentLine.endsWith("GET /forward")) {
+          Serial.println("moving forward");
+          move_forward();
+          delay(1000);
+          stop_moving();
+        } else if (currentLine.endsWith("GET /left")) {
+          Serial.println("moving left");
+          move_left();
+          delay(1000);
+          stop_moving();
+        } else if (currentLine.endsWith("GET /right")) {
+          Serial.println("moving right");
+          move_right();
+          delay(1000);
+          stop_moving();
+        } 
+      }
+    }
+    client.stop();
+  }
+}
+
+void move_forward() {
+  // Forward movement code
+  analogWrite(E1, 255); // Set speed
+  analogWrite(E2, 255); // Set speed
+  digitalWrite(M1, HIGH); // Move forward
+  digitalWrite(M2, LOW); // Move forward
+}
+
+void move_left() {
+  // Left turn code for 90-degree stationary turn
+  analogWrite(E1, 255); // Set speed for motor 1
+  analogWrite(E2, 255); // Set speed for motor 2
+  digitalWrite(M1, HIGH); // Run motor 1 forward
+  digitalWrite(M2, HIGH); // Run motor 2 backward
+
+  delay(turnDuration); // Wait for the duration of the turn
+
+  // Stop motors after turning
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, HIGH);
+}
+
+void move_right() {
+  // Right turn code for 90-degree stationary turn
+  analogWrite(E1, 255); // Set speed for motor 1
+  analogWrite(E2, 255); // Set speed for motor 2
+  digitalWrite(M1, LOW); // Run motor 1 backward
+  digitalWrite(M2, LOW); // Run motor 2 forward
+
+  delay(turnDuration); // Wait for the duration of the turn
+
+  // Stop motors after turning
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, HIGH);
+}
+void stop_moving() {
+  analogWrite(E1, 0); // Stop one motor for turning
+  analogWrite(E2, 0); // Set speed
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, HIGH);
+}
