f10ff544a6761c1e57fd05f770dfcf2bc6f0e90e
driessenslucas <lucas.driessens@student.howest.be>
2024-01-12 11:38:44 +0100

    updated:ip

--- streaming_maze_cam/maze_cnn/stream.py
+++ streaming_maze_cam/maze_cnn/stream.py
@@ -21,7 +21,7 @@ def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
 
 
 ## this is for later:
