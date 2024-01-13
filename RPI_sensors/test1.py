0530a85a17562ca06d20fc3c7928b9084e3286e7
driessenslucas <lucas.driessens@student.howest.be>
2024-01-13 11:39:52 +0100

    updated: sensor micro service

--- RPI_sensors/test1.py
+++ RPI_sensors/test1.py
@@ -10,6 +10,7 @@
 from flask import Flask, Response, render_template
 import cv2
 from gpiozero import DistanceSensor
+from time import sleep
 
 app = Flask(__name__)
 
@@ -18,15 +19,20 @@ app = Flask(__name__)
 def index():
    return "hello_world"
 
-@app.route('/sensor')
-def get_sensor_value():
-   from gpiozero import DistanceSensor
-   from time import sleep
-
-   sensor = DistanceSensor(echo=5, trigger=6)
-   print('Distance: ', sensor.distance )
-   
-   return f"{sensor.distance}"
+@app.route('/sensor/{direction}')
+def get_sensor_value(direction):
+   if direction == "front":
+      sensor = DistanceSensor(echo=5, trigger=6)
+      print('Distance: ', sensor.distance )
+      return f"{sensor.distance}"
+   elif direction == "left":
+      sensor = DistanceSensor(echo=17, trigger=27)
+      print('Distance: ', sensor.distance )
+      return f"{sensor.distance}"
+   elif direction == "right":
+      sensor = DistanceSensor(echo=23, trigger=24)
+      print('Distance: ', sensor.distance )
+      return f"{sensor.distance}"
 
 
 if __name__ == '__main__':
