39474be9cef329032847620f08653511e842c70a
driessenslucas <lucas.driessens@student.howest.be>
2024-01-15 20:15:57 +0100

    fix: script issues

--- RPI_display/service/virtual_display.sh
+++ RPI_display/service/virtual_display.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Check if the Xvfb server is already running
+if [ -e /tmp/.X99-lock ]; then
+    echo "Display :99 is already in use. Trying to release it..."
+    kill $(cat /tmp/.X99-lock)
+    # Wait a bit to ensure the process has been killed
+    sleep 2
+fi
+
+# Start Xvfb
 Xvfb :99 -screen 0 1024x768x16 &
+sleep 2 # Give some time for Xvfb to start
 export DISPLAY=:99
 
-sudo xhost +Local:docker
+# Allow local docker containers to connect to the X server
+xhost +Local:docker
