24808c192c629a95b27295326c5482901ececc0d
driessenslucas <lucas.driessens@student.howest.be>
2024-01-15 17:39:35 +0100

    fixed: sensor readings in docker container

--- main_web_app/sensors/app.py
+++ main_web_app/sensors/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, Response, render_template
 import cv2
 from gpiozero import DistanceSensor
-from gpiozero.pins.pigpio import PiGPIOFactory
+# from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 import os
+from signal import pause
 
 app = Flask(__name__)
 
-#get env variable factory = PiGPIOFactory(host='${env.host_IP}')
-host = os.getenv("HOST_IP", "192.168.0.25") 
-factory = PiGPIOFactory(host=host)
+# #get env variable factory = PiGPIOFactory(host='${env.host_IP}')
+# host = os.getenv("HOST_IP", "192.168.0.25") 
+# factory = PiGPIOFactory(host=host)
 
 @app.route('/')
 def index():
@@ -18,17 +19,19 @@ def index():
 @app.route('/sensor/<direction>')
 def get_sensor_value(direction):
    if direction == "front":
-      sensor_front = DistanceSensor(echo=5, trigger=6, pin_factory=factory)
+      sensor_front = DistanceSensor(echo=5, trigger=6)
       print('Distance: ', sensor_front.distance )
       return f"{sensor_front.distance * 100 }"
    elif direction == "left":
-      sensor_left = DistanceSensor(echo=17, trigger=27, pin_factory=factory)
+      sensor_left = DistanceSensor(echo=17, trigger=27)
       print('Distance: ', sensor_left .distance )
       return f"{sensor_left.distance  * 100}"
    elif direction == "right":
-      sensor_right = DistanceSensor(echo=24, trigger=23, pin_factory=factory)
+      sensor_right = DistanceSensor(echo=24, trigger=23)
       print('Distance: ', sensor_right.distance )
       return f"{sensor_right.distance * 100 }"
+   pause()
+
 
 
 if __name__ == '__main__':
